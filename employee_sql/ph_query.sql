cd986cc7656fe336a1b8f803cc193176bbe13754
MatchaCats <natscholarships18@gmail.com>
2024-08-18 18:29:18 -0700

    adding query

---
+++ employee_sql/ph_query.sql
@@ -0,0 +1,37 @@
+CREATE TABLE titles(
+	title_id VARCHAR(40) NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(40) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(40) NOT NULL,
+	last_name VARCHAR(40) NOT NULL,
+	sex VARCHAR(40) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE departments(
+	dept_no VARCHAR(40) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(40) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (dept_no)
+);
+
+
+
+
+
+
