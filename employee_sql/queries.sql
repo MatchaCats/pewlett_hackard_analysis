f2b61767f29018fdc46c7df654a268d51e442253
MatchaCats <natscholarships18@gmail.com>
2024-08-21 14:06:01 -0700

    5/8 queries completed

--- employee_sql/queries.sql
+++ employee_sql/queries.sql
@@ -5,8 +5,8 @@ SELECT emp.emp_no,
 	   emp.first_name,
 	   emp.sex,
 	   sal.salary
-FROM employees as emp
-	LEFT JOIN salaries as sal
+FROM employees AS emp
+	LEFT JOIN salaries AS sal
 	ON emp.emp_no = sal.emp_no
 ORDER BY emp.emp_no;
 
@@ -16,7 +16,7 @@ ORDER BY emp.emp_no;
 SELECT emp.first_name,
        emp.last_name,
 	   emp.hire_date
-FROM employees as emp
+FROM employees AS emp
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 --List the manager of each department along with their department 
@@ -27,18 +27,31 @@ SELECT deptm.dept_no,
 	   emp.first_name,
 	   emp.last_name
 FROM dept_manager AS deptm
-INNER JOIN departments as dept
+INNER JOIN departments AS dept
 ON (deptm.dept_no = dept.dept_no)
-INNER JOIN employees as emp
+INNER JOIN employees AS emp
 ON (deptm.emp_no = emp.emp_no);
 
 --List the department number for each employee along with that 
 -- employee’s employee number, last name, first name, and department name.
-
+SELECT deptm.dept_no,
+	   deptm.emp_no,
+	   emp.last_name,
+	   emp.first_name,
+	   dept.dept_name
+FROM dept_manager AS deptm
+INNER JOIN departments AS dept
+ON (deptm.dept_no = dept.dept_no)
+INNER JOIN employees AS emp
+ON (deptm.emp_no = emp.emp_no);
 
 --List first name, last name, and sex of each employee whose 
 -- first name is Hercules and whose last name begins with the letter B.
-
+SELECT emp.first_name,
+	   emp.last_name,
+	   emp.sex
+FROM employees AS emp
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 --List each employee in the Sales department, including their 
 -- employee number, last name, and first name.
